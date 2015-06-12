42a95d9ffdc4b86b8d09d05d37cf91fb92771d60
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-06-11 19:25:21 -0500

    successfully connecting, cant join a room yet

--- socket_client.py
+++ socket_client.py
@@ -4,15 +4,10 @@ import threading
 import time
 
 def onopen(ws):
-    def run(*args):
-        for i in range(3):
-            time.sleep(1)
-            ws.send('Hello {}'.format(i))
-    threading.start_new_thread(run,())
     print('Websocket open!')
 
-def onmessage(message):
-    print('Message: {}'.format(message))
+def onmessage(ws, message):
+    print(message)
 
 def on_error(ws, e):
     print(e)
@@ -21,14 +16,24 @@ if __name__ == '__main__':
     streamer_name = 'beohoff'
     # this is default for the flask app
     port_number =  5000
+    """
     website_socket= 'ws://www.watchpeoplecode.com/streamer/{}:{}/chat'.format(
             streamer_name, port_number)
-    website_url = 'http://www.watchpeoplecode.com/streamer/beohoff:{}/chat/'.format(port_number)
+    """
+    params = {'EIO':3, 'transport':'websocket'}
+    website_socket = 'ws://www.watchpeoplecode.com/streamer/beohoff:5000'
+    website_url = 'http://www.watchpeoplecode.com/socket.io/1/'
+    test_website_socket = 'ws://www.watchpeoplecode.com/socket.io/1/websocket/'
     #header = {'username':'beohoff'}
     r = requests.post(website_url)
-    print(r.text, r)
-    ws = websocket.WebSocketApp(website_socket+ '/chat',
+    key = r.text.split(':')[0]
+    """
+    ws = websocket.WebSocketApp(website_socket+ '/chat/'+key,
             on_message = onmessage)
+    """
+    ws = websocket.WebSocketApp(test_website_socket + key,
+            on_message = onmessage)
+            #subprotocols=["chat"])
     ws.on_open = onopen
     ws.on_error = on_error
     ws.run_forever()
