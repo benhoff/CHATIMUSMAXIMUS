cd1280979fbfc2d08974723391e93bf5d3fd1d19
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-06-16 11:07:02 -0500

    Cleaned up the irc client slightly. Untested

--- irc_client.py
+++ irc_client.py
@@ -1,29 +1,33 @@
 import socket
-import string
+import sys
 import os
 from threading import Thread
 from messager import Messager
+from PyQt5 import QtCore
 
-class ReadOnlyIRCBot(object):
+class ReadOnlyIRCBot(QtCore.QObject):
     HOST = 'irc.twitch.tv'
     PORT = 6667
-    
-    def __init__(self, channel, nick=None, oauth_token=None):
-        self.channel = channel
-        self.readbuffer = ""
+    chat_signal = QtCore.pyqtSignal(str, str) 
+
+    def __init__(self, channel, nick=None, oauth_token=None, parent=None):
+        super(ReadOnlyIRCBot, self).__init__(parent)
         if nick is None:
             nick = channel 
         if oauth_token is None:
             oauth_token = os.getenv('TWITCH_KEY')
-        self.oauth_token = oauth_token
         self.nick = nick
+        self.oauth_token = oauth_token
+        self.channel = channel
 
+        # NOTE: If use `split()` in run this has extra space on the end!!!
+        self.connect_message = 'tmi.twitch.tv 376 {} '.format(self.nick)
         self.socket = socket.socket()
+        self.readbuffer = ""
+
         self.socket.connect((self.HOST, self.PORT))
 
         self.connected = False
-        self._messager = Messager()
-        self.chat_signal = self._messager.chat_signal
 
         self._thread = Thread(target=self.run)
         self._thread.setDaemon(True)
@@ -40,41 +44,51 @@ class ReadOnlyIRCBot(object):
     def run(self):
         self._connect_to_server_helper()
         while True:
-            self.readbuffer = self.readbuffer + \
-                    self.socket.recv(1024).decode('UTF-8')
+            self.readbuffer += self.socket.recv(1024).decode('UTF-8')
 
-            temp = str.split(self.readbuffer, '\n')
+            temp = self.readbuffer.split('\n')
             self.readbuffer = temp.pop()
-            print('length of temp: ', len(temp))
 
+            # For DEBUGGING
+            #print('length of temp: ', len(temp))
             for line in temp:
                 line = line.rstrip()
+
                 # TODO: switch back to using just `split()`
                 line = line.split(':', 2)
-
+                
+                # Handles PING/PONG of server
                 if line[0] == "PING":
-                    print('ping!')
-                    self.socket.send(bytes("PONG {}".format(line[1]), 
-                                           "UTF-8"))
+                    # TODO: verify that this works other places than just in my head
+                    self._send_helper('PONG {}'.format(line[1]))
                     break
-                # TODO: Just parse `PRIVMSG` 
-                # NOTE: If use `split()` this has extra space on the end!!!
-                elif line[1] == 'tmi.twitch.tv 376 {} '.format(self.nick):
+                # Handles the connection method
+                elif line[1] == self.connect_message:
                     self.connected = True
                     self._send_helper('JOIN #{}'.format(self.channel))
                     break
-                test_line = line[1].split()
-                if len(test_line) > 0:
-                    if test_line[1] == 'PRIVMSG':
-                        if len(line) > 1:
-                            message = line[2]
-                        else:
-                            message = 'PLACEHOLDER'
-                        self._messager.recieve_chat_data(test_line[0], message)
+
+                # FIXME: Is there an option where there isn't an item in index of 1?
+                # code will break if so...
+                try:
+                    test_line = line[1].split()
+                    if len(test_line) > 0:
+                        if test_line[1] == 'PRIVMSG':
+                            if len(line) > 1:
+                                message = line[2]
+                            else:
+                                message = 'PLACEHOLDER'
+                            self.chat_signal.emit(test_line[0], message)
+                            break
+                        
+                except:
+                    print("Unexpected error:", sys.exc_info()[0])
+                    print('Line: ', line[1])
+                    print(test_line)
+
                 for index, i in enumerate(line):
                     print(line[index])
 
 if __name__ == '__main__':
-    irc_bot = ReadOnlyIRCBot('beohoff')
-
-
+    irc_bot = ReadOnlyIRCBot('beohoff', nick='beohoff')
+    # FIXME: Add in a loop here or something
