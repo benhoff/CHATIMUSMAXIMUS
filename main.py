c0ad8b4d55273b62fc98744b41b99f196f772442
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-06-17 10:22:50 -0500

    deleted init and moved all the logic to the main

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import sys 
+from os import path
+import json
 from PyQt5 import QtWidgets, QtNetwork, QtCore
 from gui import MainWindow
+import socket_protocols
 
-# import a couple of the instances from the __init__ file
-from . import youtube_socket, wpc_socket, irc_client
+default_filename = 'default_settings.json'
+perferred_filename = 'settings.json'
+
+if path.exists(perferred_filename):
+    filename = perferred_filename
+else:
+    filename = default_filename
+    print('Change your default settings!')
+
+with open(filename) as setting_file:    
+    settings = json.load(setting_file)
+
+chrome_server_settings = settings['chrome_tcp_server']
+youtube_socket = socket_protocols.ReadOnlyTCPSocket(chrome_server_settings['host'],
+                                                    chrome_server_settings['port'])
+
+wpc_settings = settings['watchpeoplecode']
+wpc_socket = socket_protocols.ReadOnlyWebSocket(wpc_settings['channel'])
+
+twitch_settings = settings['twitch']
+irc_client = socket_protocols.ReadOnlyIRCBot(twitch_settings['channel'], 
+                                             twitch_settings['nick'], 
+                                             twitch_settings['oauth_token'])
 
 app = QtWidgets.QApplication(sys.argv)
 
