755f3985c4e277930a6ed520ffee56d5a3bcb454
Ben Hoff <hoff.benjamin.k@gmail.com>
2016-04-23 11:21:15 -0600

    release 0.0.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(directory, 'README.rst')) as f:
 
 setup(
     name="chatimusmaximus",
-    version='0.0.5',
+    version='0.0.6',
     description='Chat GUI for youtube, twitch, livecoding, and WatchPeopleCode chats',
     # long_description=long_description,
     url='https://github.com/benhoff/chatimusmaximus',
